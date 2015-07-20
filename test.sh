b7202090173ccd8d6d029c443778bc49f50e8ed4
Vitaly Kushner <vitaly@astrails.com>
2015-07-20 15:36:26 +0200

    fix bug with long file name

--- test.sh
+++ test.sh
@@ -4,26 +4,26 @@ T=tmp/test
 
 rm -rf $T
 
-mkdir -p $T/1
-echo 123 > $T/1/123
-echo 456 > $T/1/456
+mkdir -p "$T/1"
+echo 123 > "$T/1/123"
+echo 456 > "$T/1/456"
 
-dedup index $T/i1 $T/1
+dedup index "$T/i1" "$T/1"
 
 mkdir -p $T/2
 
-echo aaa > $T/2/aaa
-echo bbb > $T/2/bbb
+echo aaa > "$T/2/aaa"
+echo bbb > "$T/2/bbb"
 
-dedup index $T/i2 $T/2
+dedup index "$T/i2" "$T/2"
 
-mkdir -p $T/3/a/b/c
-echo 123 > $T/3/a/123
-echo aaa > $T/3/a/b/aaa
-echo xxx > $T/3/a/b/c/xxx
+mkdir -p "$T/3/a A/b ' B/c C"
+echo 123 > "$T/3/a A/1 2 3"
+echo 123 > "$T/3/a A/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+echo aaa > "$T/3/a A/b ' B/a 'aa"
+echo xxx > "$T/3/a A/b ' B/c C/x xx"
 
-dedup move $T/3 $T/4 $T/i1 $T/i2
-#dedup move ~/a/astrails/oss/deduper/$T/3 $T/4 $T/i1 $T/i2
+dedup move "$T/3" "$T/4" "$T/i1" "$T/i2"
 
 echo -------------
 tree $T/4
